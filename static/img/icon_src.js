e895db13fb6ddd599c4673f7f789c19fb07e6e96
Peindre La Colline <PeindreLaColline@gmail.com>
2024-11-02 19:34:51 +0900

    refact: 이미지 소스 import 간편화

---
+++ static/img/icon_src.js
@@ -0,0 +1,13 @@
+import book_cover from "./icon/book-cover.svg";
+import book from "./icon/book.png";
+import go_back from "./icon/goback.png";
+import menu from "./icon/menu.png";
+import save from "./icon/save.svg";
+
+export {
+    book_cover,
+    book,
+    go_back,
+    menu,
+    save
+};
---
+++ static/img/profile_src.js
@@ -0,0 +1,15 @@
+import boy_blue from "./profile/boy-blue.png";
+import boy_green from "./profile/boy-green.png";
+import girl_pink from "./profile/girl-pink.png";
+import girl_purple from "./profile/girl-purple.png";
+import indecisive_kid from "./profile/indecisive-kid.png";
+import create_profile from "./profile/createprofilebutton.png";
+
+export {
+    boy_blue,
+    boy_green,
+    girl_pink,
+    girl_purple,
+    indecisive_kid,
+    create_profile,
+};
