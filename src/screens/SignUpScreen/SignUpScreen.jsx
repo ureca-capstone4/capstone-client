0b2f5abf64f1af5b430d23de02a9376a29b9dde6
Peindre La Colline <PeindreLaColline@gmail.com>
2024-11-04 11:31:32 +0900

    asd

--- src/screens/SignUpScreen/SignUpScreen.jsx
+++ src/screens/SignUpScreen/SignUpScreen.jsx
@@ -14,7 +14,7 @@ export const SignUpScreen = () => {
 
   const handleSignUp = async () => {
     try {
-      const response = await fetch(`https://${REACT_APP_BACKEND_IP}/api/v1/auth/signup`, {
+      const response = await fetch(`https://${process.env.REACT_APP_BACKEND_IP}/api/v1/auth/signup`, {
         method: "POST",
         headers:{
           "Content-Type": "application/json",
